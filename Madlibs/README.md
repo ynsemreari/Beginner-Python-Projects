ed7ee8bcf4eac2867baf08af0a2b1e1205a8795c
ynsemreari <ynsemreari@gmail.com>
2022-12-23 14:07:54 +0300

    Hikayenin bir kısmı yazıldı.

---
+++ Madlibs/README.md
@@ -0,0 +1,4 @@
+This is my first project on Python. And also it is my first file on GİTHUB.
+MADLİBS Game is a word game. There is a story. Story has blanks.
+Program asks you some verbs, nouns, adj etc. After getting the words,
+program fills in the blanks.
---
+++ Madlibs/madlibs.py
@@ -0,0 +1,28 @@
+########################
+# Mad Libs Game - Yunus Emre ARI
+########################
+noun1 = input("Noun: ")
+verb1 = input("Present time verb: ")
+noun2 = input("Noun: ")
+place1 = input("Place: ")
+activity1 = input("Activity: ")
+activity2 = input("Activity: ")
+activity3 = input("Activity: ")
+time1 = input("Time Line: ")
+verb2 = input("Past time verb: ")
+adjective1 = input("Adjective: ")
+verb3 = input("Verb: ")
+
+
+madlibs = f"It was not a Little {noun1}\nI was {verb1} with {noun2} and his family at a small, remote lake in {place1}\n" \
+          f"There were about 10 people in our group and another group of siz people in the next campsite. It\n" \
+          f"was  {time1} and both groups were doing typical activities: {activity1}, {activity2} and\n" \
+          f"{activity3}, when we all {verb2} what sounded like a little {noun1} yelling out for help. Neither group\n" \
+          f"had children  with them, but we were all {adjective1} we were hearing a little {noun1} and decided to {verb3}\n" \
+          f"the area we heard the  noises from together. There was a field behind our campsites, and we all saw a very tall,\n" \
+          f"pure white figure standing maybe 100ft away from us in the field, making the noises. We all agreed this thing\n" \
+          f"looked maybe 6 feet tall, skinny, and white as can be. We made our way closer to investigate, but whatever it \n" \
+          f"was that we saw started backing off as we got closer, and it disappeared into the trees. All night we continued\n" \
+          f"to hear a little girl calling for help as we tried to sleep. "
+
+print(madlibs)
